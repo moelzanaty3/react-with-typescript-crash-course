0df37471b7c45bb46db806063f2f05508bee6498
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-11-21 13:08:17 +0200

    handling state and fetching users from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import IUser from './interfaces/IUser'
+import Loading from './components/Loading'
+
 function App() {
+  const [users, setUsers] = useState<IUser[] | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  const fetchUsers = async (numberOfUsers: number = 1) => {
+    try {
+      setLoading(true)
+      const response = await axios.get(
+        `https://randomuser.me/api/?results=${numberOfUsers}&nat=us,ca&noinfo`
+      )
+      const users = response.data.results
+      setUsers(users)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    const fetchData: () => Promise<void> = async () => {
+      await fetchUsers()
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">Users List</header>
+      <main>
+        {loading && <Loading />}
+        {users && (
+          <pre>
+            <code>{JSON.stringify(users, null, 2)}</code>
+          </pre>
+        )}
+      </main>
     </div>
   )
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = (): JSX.Element => (
+  <div className="loading">
+    <img
+      src="https://cdn.dribbble.com/users/4221/screenshots/2847133/loading.gif"
+      alt="users loading"
+    />
+  </div>
+)
+export default Loading
