7d05b64f250278167ee7c4199b2e4817d177c3e7
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-11-21 14:10:05 +0200

    add search functionality to the users

--- src/containers/UsersList.tsx
+++ src/containers/UsersList.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { sortBy } from 'sort-by-typescript'
 import User from '../components/User'
 import IUser from '../interfaces/IUser'
 
@@ -8,11 +10,45 @@ type IProps = {
 
 // const UsersList = ({ users }: IProps): JSX.Element => {
 const UsersList: React.FC<IProps> = ({ users }): JSX.Element => {
+  const [query, setQuery] = useState<string>('')
+  const showingUsers: IUser[] =
+    query === ''
+      ? users
+      : users.filter((u: IUser): boolean => {
+          const name: string = u.name.first + ' ' + u.name.last
+          return name.toLowerCase().includes(query.trim().toLowerCase())
+        })
   return (
     <div className="list-users">
+      <div className="list-users-top">
+        <input
+          className="search-users"
+          type="text"
+          placeholder="Search Users"
+          value={query}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
+            const value: string = e.target.value
+            setQuery(value)
+          }}
+        />
+      </div>
+      {showingUsers.length !== users.length && (
+        <div className="showing-users">
+          <span>
+            Now showing {showingUsers.length} of {users.length}
+          </span>
+          <button
+            onClick={() => {
+              setQuery('')
+            }}
+          >
+            Show all
+          </button>
+        </div>
+      )}
       <div className="row users-container">
-        {users &&
-          users.map(
+        {showingUsers &&
+          showingUsers.sort(sortBy('name.first')).map(
             (user: IUser): JSX.Element => (
               <div className="col-12 col-md-6" key={user.login.uuid}>
                 <User user={user} />
